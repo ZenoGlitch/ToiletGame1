a11bffa96e7f1c55811d5d830fc6d23378469891
Sebastian <sebastian.norderang.6229@student.uu.se>
2025-04-08 00:16:58 +0200

    Basic player controls and world environment with directional light

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,41 +1,39 @@
-extends Node3D
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
 @onready var pivot = $CameraPivot
 @onready var camera = $CameraPivot/Camera3D
+var mouseSensitivity : float = 0.01
 
-const mouseSensitivity : float = 0.001
-var velocity : float
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _unhandled_input(event: InputEvent):
-
+func _unhandled_input(event):
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			pivot.rotate_y(-event.relative.x * mouseSensitivity)
 			camera.rotate_x(-event.relative.y * mouseSensitivity)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 
-			#var lookSidewaysAngle = 30
-			#var lookUpAngle = 30
-			#var lookDownAngle = 30
-			#
-			#pivot.rotation.y = clamp(pivot.rotation.y, deg_to_rad(-lookSidewaysAngle), deg_to_rad(lookSidewaysAngle))
-			#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-lookDownAngle), deg_to_rad(lookUpAngle))
 
-func _input(event: InputEvent):			
-	if event.is_action_pressed("MoveForward"):
-		
-		pass
-	if event.is_action_pressed("MoveBackwards"):
-		pass
-	if event.is_action_pressed("MoveLeft"):
-		pass
-	if event.is_action_pressed("MoveRight"):
-		pass
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBackwards")
+	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
